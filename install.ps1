7e986e8f7f3eb02f1a534d3b6945fc3e6cfb60e8
Theo van Drimmelen <theo@vandrimmelen.eu>
2018-11-22 17:30:56 +0100

    Ported to latest webwhatsapi version + python3

--- install.ps1
+++ install.ps1
@@ -1,16 +1,27 @@
 cd ${env:HOMEPATH}\Downloads
 
+#Enable TLS12, TLS11 and TLS
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+Write-output "Prior to running this script, install visual studio build tools from https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=15#"
+$answer = Read-Host -Prompt "Type 'yes' when installation is completed"
+
+if ($answer -ne "yes")
+{
+    Exit
+}
+
 Write-output "Installing Python..."
-invoke-webrequest https://www.python.org/ftp/python/2.7.14/python-2.7.14.amd64.msi -outfile python-2.7.14.amd64.msi
-start-process msiexec -ArgumentList "/i", "python-2.7.14.amd64.msi", "/quiet" -Wait
+invoke-webrequest https://www.python.org/ftp/python/3.7.1/python-3.7.1-amd64.exe -outfile python-3.7.1-amd64.exe
+start-process python-3.7.1-amd64.exe -ArgumentList "/quiet", "InstallAllUsers=1", "PrependPath=1" -Wait
 
 Write-output "Installing Firefox..."
-invoke-webrequest "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US" -outfile firefox-setup.exe
+invoke-webrequest "https://ftp.mozilla.org/pub/firefox/releases/59.0.3/win64/en-US/Firefox%20Setup%2059.0.3.exe" -outfile firefox-setup.exe
 Start-Process ./firefox-setup.exe -ArgumentList "-ms" -wait
 
 Write-output "Installing geckodriver..."
-invoke-webrequest https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-win64.zip -outfile  geckodriver-v0.19.1-win64.zip
-Expand-Archive geckodriver-v0.19.1-win64.zip -DestinationPath C:\python27
+invoke-webrequest https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-win64.zip -outfile  geckodriver-v0.23.0-win64.zip
+Expand-Archive geckodriver-v0.23.0-win64.zip -DestinationPath "C:\Program Files\Python37"
 
 Write-output "Installing POTM..."
 $potsfolder="C:\Player of the match"
@@ -20,11 +31,14 @@ New-Item -ItemType directory -Path ${potsfolder}\input
 New-Item -ItemType directory -Path ${potsfolder}\output
 invoke-webrequest https://raw.githubusercontent.com/jpammerlaan/playerofthematch/master/run.py -outfile ${potsfolder}\run.py
 
-cd C:\Python27
+cd "C:\Program Files\Python37"
 
-Write-output "Installing Installing python libraries..."
-.\python.exe -m pip install webwhatsapi==1.0.3
+Write-output "Installing  python libraries..."
+.\python.exe -m pip install webwhatsapi
 .\python.exe -m pip install fuzzywuzzy
+
+Write-Output "Patching webwhatsapi"
+invoke-webrequest "https://raw.githubusercontent.com/mukulhase/WebWhatsapp-Wrapper/master/webwhatsapi/js/wapi.js" -OutFile "C:\Program Files\Python37\Lib\site-packages\webwhatsapi\js\wapi.js"
 
 write-output "Finished"
 write-output "Add folder ${potsfolder} to the path"
--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import time
 from fuzzywuzzy import fuzz, process
 import csv
+import operator
 
 ### Utility functions   
 def ensure_dir(file_path):
@@ -12,7 +13,7 @@ def ensure_dir(file_path):
         os.mkdir(file_path)
 
 def get_player_list():
-    with open('input/player_list.csv', 'rU') as f:
+    with open('input/player_list.csv', 'r') as f:
         return f.read().splitlines()
         
 def write_to_csv(file, contents):
@@ -24,7 +25,7 @@ def write_to_csv(file, contents):
 def write_player_tallies(output_folder, player_tallies):
     ensure_dir(output_folder)
     output_filename = './' + output_folder + '/player_tallies_' + datetime.now().strftime('%H-%M-%S')  + '.csv'
-    write_to_csv(output_filename, player_tallies.items())
+    write_to_csv(output_filename, sorted(player_tallies.items(), key=operator.itemgetter(1), reverse=True))
 
 def write_submission_log(output_folder, submission_log):
     ensure_dir(output_folder)
@@ -32,7 +33,7 @@ def write_submission_log(output_folder, submission_log):
     write_to_csv(output_filename, submission_log.items())
 
 ### Parameters
-match_name = raw_input('Voer de wedstrijd in:')
+match_name = input('Voer de wedstrijd in:')
 start_time = datetime.now()
 output_folder = 'output/' + start_time.strftime('%Y-%m-%d') + match_name
 sleep_time = 60
@@ -44,27 +45,30 @@ player_tallies = {player: 0 for player in player_options}
 
 # Main loop
 driver = WhatsAPIDriver()
+driver.wait_for_login()
+
 try:
     while True:
-        messages_by_sender = driver.view_unread()
-        print "{} berichten ontvangen. Verwerken...".format(len(messages_by_sender))
+        messages_by_sender = driver.get_unread()
+        print('{} berichten ontvangen. Verwerken...'.format(len(messages_by_sender)))
         for contact in messages_by_sender:
-            for message in contact['messages']:
-                best_player_match, match_value = process.extractOne(message['message'], player_options)
+            for message in contact.messages:
+                best_player_match, match_value = process.extractOne(message.content, player_options)
+                print('Message received from: {}. Match value: {}. Text: {}'.format(message.sender.id["user"], match_value, message.content))
                 if (match_value > match_threshold):
-                    if (contact[u'id'] not in submission_log.keys()):
+                    if (message.sender.id["user"] not in submission_log.keys()):
                         #print "Bedankt voor het stemmen op The Erima Player Of The Match! Je hebt gestemd op {}. Je kunt niet nog een keer stemmen.".format(best_player_match)
                         player_tallies[best_player_match] += 1
-                        submission_log[contact['id']] = (message['message'], best_player_match, match_value)
+                        submission_log[message.sender.id["user"]] = (message.content, best_player_match, match_value)
 
         write_player_tallies(output_folder, player_tallies)
         write_submission_log(output_folder, submission_log)
-        print "Logs geschreven."
-        print "Wachten voor {} seconden...".format(sleep_time)
+        print('Logs geschreven.')
+        print('Wachten voor {} seconden...'.format(sleep_time))
         time.sleep(sleep_time)
 except KeyboardInterrupt:
-    print "Onderbroken door gebruiker. Laatste records wegschrijven..."
+    print('Onderbroken door gebruiker. Laatste records wegschrijven...')
     write_player_tallies(output_folder, player_tallies)
     write_submission_log(output_folder, submission_log)
-    print "Logs geschreven."
-    print "Klaar! Succesvol afgesloten."
+    print('Logs geschreven.')
+    print('Klaar! Succesvol afgesloten.')
